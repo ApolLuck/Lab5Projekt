12ac730f45787dce3aaaaf9c70400ca39aa71770
A P <ap88335@stud.uph.edu.pl>
2025-01-25 19:16:47 +0100

    działa zliczanie ceny

--- src/main/resources/static/css/js/script.js
+++ src/main/resources/static/css/js/script.js
@@ -1,3 +1,4 @@
+calculateOrderValue()
 function increaseValue() {
     var value = parseInt(document.getElementById('number').value, 10);
     value = isNaN(value) ? 0 : value;
@@ -19,3 +20,38 @@ document.getElementById('number').addEventListener('input', function () {
         this.value = 1;
     }
 });
+
+function calculateOrderValue() {
+    var basePrice = parseFloat(document.getElementById('base-price').textContent);
+    var orderValue = basePrice;
+
+    var sizeFamilijna = document.getElementById('size-familijna').checked;
+    var sizeDuza = document.getElementById('size-duza').checked;
+    var sizeMala = document.getElementById('size-mala').checked;
+    var grubeCiasto = document.querySelector('input[name="grube_ciasto"]:checked').value === "true";
+    var dodatkowySer = document.querySelector('input[name="dodatkowy_ser"]:checked').value === "true";
+    var coverType = document.querySelector('input[name="coverType"]:checked');
+
+    if (grubeCiasto) {
+        orderValue += 5;
+    }
+
+    if (dodatkowySer) {
+        orderValue += 5;
+    }
+
+    if (coverType && coverType.value !== "1") {
+        orderValue += 5;
+    }
+
+    if (sizeFamilijna) {
+        orderValue *= 2;
+    } else if (sizeDuza) {
+        orderValue *= 1.5;
+    }
+
+    orderValue *= parseInt(document.getElementById('number').value, 10);
+
+    document.getElementById('order-value').textContent = 'Wartość zamówienia: '
+        + orderValue.toFixed(2) + ' zł';
+}
